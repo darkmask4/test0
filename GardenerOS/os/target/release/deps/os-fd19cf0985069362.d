b29f7f7b45bbda451268e469af820762e9fbe626
darkmask4 <131189232+darkmask4@users.noreply.github.com>
2023-11-16 20:38:35 +0800

    Add files via upload

---
+++ GardenerOS/os/target/release/deps/os-fd19cf0985069362.d
@@ -0,0 +1,5 @@
+/mnt/os/target/release/deps/os-fd19cf0985069362: src/main.rs
+
+/mnt/os/target/release/deps/os-fd19cf0985069362.d: src/main.rs
+
+src/main.rs:
---
+++ GardenerOS/os/target/release/os.d
@@ -0,0 +1,1 @@
+/mnt/os/target/release/os: /mnt/os/src/main.rs
