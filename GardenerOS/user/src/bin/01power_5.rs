73a344aac5694c45b8e5de14b22bebaf691a8197
darkmask4 <131189232+darkmask4@users.noreply.github.com>
2024-01-05 19:34:20 +0800

    Add files via upload

---
+++ GardenerOS/user/src/bin/01power_5.rs
@@ -0,0 +1,28 @@
+#![no_std]
+#![no_main]
+
+#[macro_use]
+extern crate user_lib;
+
+const LEN: usize = 100;
+
+#[no_mangle]
+fn main() -> i32 {
+    let base = 5u64;  // 修改为计算 5 的幂
+    let m = 998244353u64;
+    let iter: usize = 200000;
+    let mut s = [0u64; LEN];
+    let mut cur = 0usize;
+    s[cur] = 1;
+    for i in 1..=iter {
+        let next = if cur + 1 == LEN { 0 } else { cur + 1 };
+        s[next] = s[cur] * base % m;  // 修改为计算 5 的幂
+        cur = next;
+        if i % 10000 == 0 {
+            println!("power_5 [{}/{}]", i, iter);
+        }
+    }
+    println!("{}^{} = {}", base, iter, s[cur]);  // 修改为计算 5 的幂
+    println!("Test power_5 OK!");
+    0
+}
